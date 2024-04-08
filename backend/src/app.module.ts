c16de4b2ecf46dfdcc8904919a77c87fa76953ae
zshnb <a857681664@gmail.com>
2024-04-08 11:17:23 +0800

    feat: setup backend config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DomainModule } from './domain/domain.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DomainModule],
+  imports: [
+    DomainModule,
+    ConfigModule.forRoot({
+      envFilePath: [`config/.env.${process.env.NODE_ENV}`],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
