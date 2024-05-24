7158f5ffa7b187a71a4c96bdf16fec94b226ec61
zshnb <a857681664@gmail.com>
2024-05-24 17:25:42 +0800

    frontend:fix: i18n on metadata

--- apps/web/src/app/[locale]/layout.tsx
+++ apps/web/src/app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import { Inter as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
 import {ReactNode} from "react";
 import { useTranslation } from "@/app/i18n";
+import { headers } from "next/headers";
 export const runtime = 'edge';
 
 const fontSans = FontSans({
@@ -17,12 +18,12 @@ export async function generateStaticParams() {
   return languages.map((locale) => ({ locale }))
 }
 
-export default async function RootLayout({ children, params }: {children: ReactNode, params: {
-  lng: string
-}}) {
-  const {t} = await useTranslation(params.lng)
+export default async function RootLayout({ children }: {children: ReactNode}) {
+  const header = headers()
+  const language = header.get('next-url')?.slice(1)
+  const {t} = await useTranslation(language)
   return (
-    <html lang={params.lng} suppressHydrationWarning>
+    <html lang={language} suppressHydrationWarning>
     <head>
       <link rel="icon" href="/apps/web/src/app/icon.png" sizes="any" />
       <title>{t('metadata.title')}</title>
